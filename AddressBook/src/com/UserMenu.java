430104ae7a7a5a85bc3fbec06482cdb538082636
Naveen-2 <naveen2k15cse@gmail.com>
2022-03-18 22:59:51 +0530

    Multiple addressbooks can be created with unique names

---
+++ AddressBook/src/com/UserMenu.java
@@ -0,0 +1,14 @@
+package com;
+
+import java.util.Scanner;
+
+public class UserMenu {
+    Scanner input = new Scanner(System.in);
+
+    public int showMainMenu() {
+        System.out.println("Main Menu:\n 1.Add Contact \n 2.Edit Contact \n 3.Delete Contact \n 4.Add Multiple Contacts\n 5.Add new Address Book");
+        System.out.print("Enter your choice: ");
+        return input.nextInt();
+    }
+
+}
